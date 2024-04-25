524a6144a2aca7b213ab50cadce15fc407b7cf84
Sayem <sayem465508@gmail.com>
2024-04-25 13:05:11 +0600

    created a users route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,8 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from '../App';
+import Users from "../components/Users";
 export const routes = createBrowserRouter([
     {
         path: '/',
         element: <App />
+    },
+    {
+        path: '/users',
+        element: <Users />
     }
 ])
