4f18d0ec28cc384550f8ff9ffa0c75200d890d08
Sayem <sayem465508@gmail.com>
2024-04-25 13:00:10 +0600

    added user to the database

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css'
+
+function App() {
+
+  const handleAddUser = e => {
+    e.preventDefault();
+    const from = e.target;
+
+    const name = from.name.value;
+    const email = from.email.value;
+    const user = {name, email}
+    console.log("new users", user);
+
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      if(data.insertedId){
+        alert('user added');
+        from.reset()
+      }
+    })
+
+
+   
+  }
+
+  return (
+    <>
+      
+      <h2>Simple CRUD</h2>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" />
+        <br />
+        <input type="email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add" />
+      </form>
+      
+    </>
+  )
+}
+
+export default App
